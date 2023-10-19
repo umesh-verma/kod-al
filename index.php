df2ac8223872e137672079ad118664ac25c576e8
madhukarmate <148354257+madhukarmate@users.noreply.github.com>
2023-10-19 15:03:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	ob_start();
 	include(dirname(__FILE__).'/config/config.php');
 	$app = new Application();
-	//$app->setDefault('user.index.index');
-	//$app->run(); 
+	$app->setDefault('user.index.index');
+	$app->run(); 
 
 ?>
